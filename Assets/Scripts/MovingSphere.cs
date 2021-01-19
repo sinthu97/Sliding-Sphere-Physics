429a2bbd824788bc932fcdf98f3618a09756717a
sinthu97 <47841514+sinthu97@users.noreply.github.com>
2021-01-19 12:09:53 +0530

    3rd commit

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -30,6 +30,12 @@ public class MovingSphere : MonoBehaviour
     [SerializeField]
     LayerMask probeMask = -1;
 
+    [SerializeField]
+    Transform playerInputSpace = default;
+
+    [RequireComponent(typeof(Camera))]
+    public class OrbitCamera : MonoBehaviour { }
+
     Vector3 velocity, desiredVelocity;
     Rigidbody body;
     bool desiredJump;
@@ -62,7 +68,21 @@ public class MovingSphere : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        if (playerInputSpace)
+        {
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
+        }
+        else
+        {
+            desiredVelocity =
+                new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        }
         desiredJump |= Input.GetButtonDown("Jump");
         GetComponent<Renderer>().material.SetColor(
             "_Color", OnGround ? Color.black : Color.white
